3881d173d80fcd7410e4526dd5312e9282dac9a3
shaunsephton <shaunsephton@gmail.com>
2010-05-18 15:12:13 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-event',
+    version='dev',
+    description='Django event app.',
+    author='Praekelt Consulting',
+    author_email='dev@praekelt.com',
+    url='https://github.com/praekelt/django-event',
+    packages = find_packages(),
+    include_package_data=True,
+    ],
+)
