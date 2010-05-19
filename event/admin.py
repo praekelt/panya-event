f3b4f719e9ee9522bf4c14755a7db9133fe831d0
jonathan <jonathan@qoda-server.(none)>
2010-05-19 12:19:38 +0200

    modified admin classes to suite chances in models

--- event/admin.py
+++ event/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 
 from content.admin import ModelBaseAdmin
-from event.models import Event, Location, Venue
+from event.models import Appearance, Event, Location, Venue
 
-class VenueInline(admin.TabularInline):
-    model = Venue
+class AppearanceInline(admin.TabularInline):
+    model = Appearance
 
 class EventAdmin(ModelBaseAdmin):
     inlines = [
-        VenueInline,
+        AppearanceInline,
     ]
     
 admin.site.register(Event, EventAdmin)
 admin.site.register(Location)
+admin.site.register(Venue)
