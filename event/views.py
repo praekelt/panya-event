876f8f27f8e0bd844ebfbf40a6a15d04e1cbaa7d
jonathan <jonathan@qoda-server.(none)>
2010-05-20 12:53:24 +0200

    modified list view class name

--- event/views.py
+++ event/views.py
@@ -2,10 +2,11 @@ from cal.models import EntryItem
 from content.generic.views import GenericObjectList, GenericObjectDetail
 from content.models import ModelBase
 from event.models import Event
+from pagemenu.pagemenus import IntegerFieldRangePageMenu
 
-class EventEntryItemList(GenericObjectList):
+class ObjectList(GenericObjectList):
     def get_extra_context(self, *args, **kwargs):
-        extra_context = super(EventEntryItemList, self).get_extra_context(*args, **kwargs)
+        extra_context = super(ObjectList, self).get_extra_context(*args, **kwargs)
         added_context = {'title': 'Events'}
         if extra_context:
             extra_context.update(
@@ -16,19 +17,19 @@ class EventEntryItemList(GenericObjectList):
 
         return extra_context
     
-    def get_template_name(self):
-        return 'event/event_entryitem_list.html'
-    
     def get_pagemenu(self, request, queryset):
         return None
+
+    def get_paginate_by(self):
+        return 12
     
     def get_queryset(self):
         return EntryItem.permitted.by_model(Event).order_by('start')
-        
-    def get_url_callable(self):
-        return ''
+    
+    def get_template_name(self):
+        return 'event/event_entryitem_list.html'
 
-object_list = EventEntryItemList()
+object_list = ObjectList()
 
 class ObjectDetail(GenericObjectDetail):
     def get_extra_context(self, slug, *args, **kwargs):
@@ -44,9 +45,6 @@ class ObjectDetail(GenericObjectDetail):
             extra_context = added_context
 
         return extra_context
-   
-    def get_template_name(self):
-        return 'event/event_detail.html'
     
     def get_pagemenu(self, request, queryset):
         return None
