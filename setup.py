aba93702cfec6d03002fc228cae29fa4bb311d9b
shaunsephton <shaunsephton@gmail.com>
2010-06-14 13:52:58 +0200

    panya rename

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-event',
-    version='dev',
-    description='Django event app.',
-    author='Praekelt Consulting',
+    name='panya-event',
+    version='0.0.1',
+    description='Panya event app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-event',
+    license='BSD',
+    url='http://github.com/praekelt/panya-event',
     packages = find_packages(),
+    install_requires = [
+        'panya',
+    ],
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Panya",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
