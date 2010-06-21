841aa0d08f22ef913de7acc90b24f04148010a21
jonathan <jonathan@qoda-server.(none)>
2010-06-21 15:25:11 +0200

    removed Appearances

--- event/admin.py
+++ event/admin.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
 
 from panya.admin import ModelBaseAdmin
-from event.models import Appearance, Event, Location, Venue
+from event.models import Event, Location, Venue
 
-class AppearanceInline(admin.TabularInline):
-    model = Appearance
-
-class EventAdmin(ModelBaseAdmin):
-    inlines = [
-        AppearanceInline,
-    ]
-    
-admin.site.register(Event, EventAdmin)
+admin.site.register(Event, ModelBasAdmin)
 admin.site.register(Location)
 admin.site.register(Venue)
