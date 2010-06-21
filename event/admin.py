b8b1f45dd63663263213e0c732d4b79cd116b766
jonathan <jonathan@qoda-server.(none)>
2010-06-21 15:37:53 +0200

    fixed bug

--- event/admin.py
+++ event/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from panya.admin import ModelBaseAdmin
 from event.models import Event, Location, Venue
 
-admin.site.register(Event, ModelBasAdmin)
+admin.site.register(Event, ModelBaseAdmin)
 admin.site.register(Location)
 admin.site.register(Venue)
